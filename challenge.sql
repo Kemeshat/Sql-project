89d70698d805cebe1ee96d2307527c573c2561ce
Kemeshat <kemeshat@usf.edu>
2026-04-17 13:10:49 -0400

    Top 5 customers with highest lifetime spend

--- challenge.sql
+++ challenge.sql
@@ -1,1 +1,12 @@
-SELECT name FROM sqlite_master WHERE type='table';
+--TASK 1: Top 5 Customers with highest lifetime spend
+
+SELECT
+     c.first_name || ' ' || c.last_name AS customer_name,
+     SUM (oi.quantity * oi.unit_price) AS lifetime_spend
+FROM
+        customers c
+        JOIN orders o ON c.id = o.customer_id
+        JOIN order_items oi ON o.id = oi.order_id
+GROUP BY c.id, c.first_name, c.last_name
+ORDER BY lifetime_spend DESC
+LIMIT 5;
